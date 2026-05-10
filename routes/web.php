d35e6dde7864f5033df1652c08aac19a1d4f2ddc
Baptiste ROY <baptisteroy.co@gmail.com>
2026-05-10 18:48:23 +0200

    refactor(route): Refactored routes to use the correct controllers and improve middleware logic, with progressive updates based on project advancemen

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,30 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RankingController;
 use App\Http\Controllers\JobController;
 
-Route::get('/', function () {
-    return auth()->check() ? redirect()->route('feed') : view('welcome');
-})->name('home');
+
+Route::middleware('web')->group(function () {
+
+    Route::get('/', [FeedController::class, 'index'])->name('feed');
+});
+
 
 Route::middleware(['auth'])->group(function () {
 
-    Route::get('/feed', [FeedController::class, 'index'])->name('feed');
+    // Route::get('/profile/{user}', [ProfileController::class, 'show'])->name('profile.show');
+    // Route::get('/profile/{user}/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile/{user}', [ProfileController::class, 'update'])->name('profile.update');
 
-    Route::get('/profile/{user}', [ProfileController::class, 'show'])->name('profile.show');
-    Route::get('/profile/{user}/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile/{user}', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::get('/ranking', [RankingController::class, 'index'])->name('ranking');
 
-    Route::get('/ranking', [RankingController::class, 'index'])->name('ranking');
+    // Route::get('/jobs', [JobController::class, 'index'])->name('jobs');
 
-    Route::get('/jobs', [JobController::class, 'index'])->name('jobs');
+    // Route::get('/generator', function () {
+    //     return view('generator.index');
+    // })->name('generator');
 
-    Route::get('/generator', function () {
-        return view('generator.index');
-    })->name('generator');
-
-    Route::get('/settings', function () {
-        return view('settings.index');
-    })->name('settings');
+    // Route::get('/settings', function () {
+    //     return view('settings.index');
+    // })->name('settings');
 });
 
-require __DIR__ . '/auth.php';
+// require __DIR__ . '/auth.php';
